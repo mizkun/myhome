ef6451e597364c9ba9792e3898dfbac33804fe5d
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-28 22:35:03 +0900

    Create demo room to select wall paper.

---
+++ Assets/ChangeWalls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWalls : MonoBehaviour {
+	private Clicker cliker = new Clicker ();
+	public GameObject[] Walls;
+	public Material[] WallMaterials;
+
+	int material_index = 0;
+
+	void Start () {
+		Walls[0].GetComponent<Renderer> ().material = WallMaterials [material_index];
+		Walls[1].GetComponent<Renderer> ().material = WallMaterials [material_index];
+	}
+	
+	void Update () {
+		if (cliker.clicked ()) {
+			material_index++;
+			if (material_index > WallMaterials.Length) {
+				material_index = 0;
+			}
+			Walls [0].GetComponent<Renderer> ().material = WallMaterials [material_index];
+			Walls [1].GetComponent<Renderer> ().material = WallMaterials [material_index];
+		}
+	}
+}
---
+++ Assets/HeadLookWalk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLookWalk : MonoBehaviour {
+	public float velocity = 0.7f;
+	public bool isWalking = false;
+	public GvrAudioSource walkingSound;
+
+	private CharacterController controller;
+	private Clicker cliker = new Clicker ();
+
+	void Start () {
+		controller = GetComponent<CharacterController> ();	
+	}
+
+	void Update () {
+		if (cliker.clicked()) {
+			toggleWalking ();
+		}
+		if (isWalking) {
+			controller.SimpleMove (Camera.main.transform.forward * velocity);
+		} else {
+			walkingSound.Stop ();
+		}
+	}
+
+	public void toggleWalking () {
+		isWalking = !isWalking;
+		if (isWalking) {
+			walkingSound.Play ();
+		} 
+	}
+}
